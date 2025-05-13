abecd6aea3d52a873313daa808c57e31a775bfb3
rick <137065156+c418cz5@users.noreply.github.com>
2025-05-13 12:59:35 +0930

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const ships = [
+var ships = [
   {
     name: "Enterprise",
-    image: "https://raw.githubusercontent.com/c418cz5/inft1016/main/%E4%BC%81%E4%B8%9A%E5%8F%B7.jpg",
+    image: "https://github.com/c418cz5/inft1016/blob/main/%E4%BC%81%E4%B8%9A%E5%8F%B7.jpg?raw=true",
     wiki: "https://en.wikipedia.org/wiki/USS_Enterprise_(CV-6)"
   },
   {
     name: "Bismarck",
-    image: "https://raw.githubusercontent.com/c418cz5/inft1016/main/%E4%BF%BE%E6%96%AF%E9%BA%A6.jpg",
+    image: "https://github.com/c418cz5/inft1016/blob/main/%E4%BF%BE%E6%96%AF%E9%BA%A6.jpg?raw=true",
     wiki: "https://en.wikipedia.org/wiki/German_battleship_Bismarck"
   },
   {
     name: "San Diego",
-    image: "https://raw.githubusercontent.com/c418cz5/inft1016/main/%E5%9C%A3%E5%9C%B0%E4%BA%9A%E5%93%A5.jpg",
+    image: "https://github.com/c418cz5/inft1016/blob/main/%E5%9C%A3%E5%9C%B0%E4%BA%9A%E5%93%A5.jpg?raw=true",
     wiki: "https://en.wikipedia.org/wiki/USS_San_Diego_(CL-53)"
   },
   {
     name: "Yamato",
-    image: "https://raw.githubusercontent.com/c418cz5/inft1016/main/%E5%A4%A7%E5%92%8C.jpg",
+    image: "https://github.com/c418cz5/inft1016/blob/main/%E5%A4%A7%E5%92%8C.jpg?raw=true",
     wiki: "https://en.wikipedia.org/wiki/Japanese_battleship_Yamato"
   },
   {
     name: "New Jersey",
-    image: "https://raw.githubusercontent.com/c418cz5/inft1016/main/%E6%96%B0%E6%B3%BD%E8%A5%BF.jpg",
+    image: "https://github.com/c418cz5/inft1016/blob/main/%E6%96%B0%E6%B3%BD%E8%A5%BF.jpg?raw=true",
     wiki: "https://en.wikipedia.org/wiki/USS_New_Jersey_(BB-62)"
   }
 ];
