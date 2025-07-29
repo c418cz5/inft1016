2dd5f19f57c8a144f33677b4d28aee72992d1d9e
Nina Nyback <107988036+ninanyb@users.noreply.github.com>
2025-07-29 12:50:17 +0930

    Update coin.py
    coin class file

--- OOP/coin.py
+++ OOP/coin.py
@@ -1,1 +1,20 @@
-
+import random
+class Coin:
+  def __init__(self):
+    self.set_value()
+    return
+  def get_value(self):
+    return self.__value
+    
+  def set_value(self):
+    self.__value = random.randint(0,1)
+    return
+    
+  def toss(self):
+    self.set_value()
+    return self.get_value()
+  pass
+coin = Coin()
+print(coin.toss())
+print(coin.toss())
+print(coin.toss())
